8745b8f9f2130da468f638ed737de5cb8dd84363
clonalejandro <contact@clonalejandro.me>
2017-12-11 21:46:02 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,19 @@
+/**
+ * Created by alejandrorioscalera
+ * On 27/9/17
+ *
+ * -- SOCIAL NETWORKS --
+ *
+ * GitHub: https://github.com/clonalejandro or @clonalejandro
+ * Website: https://clonalejandro.me/
+ * Twitter: https://twitter.com/clonalejandro11/ or @clonalejandro11
+ * Keybase: https://keybase.io/clonalejandro/
+ *    
+ * -- LICENSE --
+ *
+ * All rights reserved for clonalejandro ©traductorBinario 2017 / 2018
+**/
+
 /** SMALL CONSTRUCTORS **/
 
 var it=0;
